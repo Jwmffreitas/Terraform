79f638c185c9e7280a94beafb701b48d8144ae57
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-08-29 09:57:01 -0300

    Starting terraform

---
+++ site-estatico/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "us-west-2"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "terraform-aws-pessoal"
+}
+
+variable "domain" {
+  type        = string
+  description = ""
+  default     = ""
+}
