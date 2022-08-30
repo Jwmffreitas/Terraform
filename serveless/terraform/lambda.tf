92950661a90dcf2491be44026048217b013e3370
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-08-30 10:01:14 -0300

    Creating SNS

--- serveless/terraform/lambda.tf
+++ serveless/terraform/lambda.tf
@@ -35,6 +35,12 @@ resource "aws_lambda_function" "s3" {
 
   layers = [aws_lambda_layer_version.joi.arn]
 
+  environment {
+    variables = {
+      TOPIC_ARN = aws_sns_topic.this.arn
+    }
+  }
+
   tags = local.common_tags
 }
 
@@ -77,4 +83,12 @@ resource "aws_lambda_permission" "dynamo" {
   function_name = aws_lambda_function.dynamo.arn
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:*/*"
+}
+
+resource "aws_lambda_permission" "sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dynamo.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.this.arn
 }
