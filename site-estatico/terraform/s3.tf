575b53e4e46fc1f470daa164f8bd64427a7b4d46
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-08-29 21:22:51 -0300

    Updating bucket policy

--- site-estatico/terraform/s3.tf
+++ site-estatico/terraform/s3.tf
@@ -2,6 +2,7 @@ data "template_file" "s3-public-policy" {
   template = file("policy.json")
   vars = {
     bucket_name = local.domain
+    cdn_oai     = aws_cloudfront_origin_access_identity.origin_access_identity.id
   }
 }
 
