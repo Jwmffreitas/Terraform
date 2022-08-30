b8fbaab562b458c47bf15bb348ac6a071fa0c285
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-08-30 09:35:58 -0300

    Dynamo Lambda function created

--- serveless/terraform/lambda.tf
+++ serveless/terraform/lambda.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_layer_version" "joi" {
 data "archive_file" "s3" {
   type        = "zip"
   source_file = "${local.lambdas_path}/s3/index.js"
-  output_path = "../trash/s3-artefact.zip"
+  output_path = "files/s3-artefact.zip"
 }
 
 resource "aws_lambda_function" "s3" {
@@ -36,4 +36,29 @@ resource "aws_lambda_function" "s3" {
   layers = [aws_lambda_layer_version.joi.arn]
 
   tags = local.common_tags
+}
+
+data "archive_file" "dynamo" {
+  type        = "zip"
+  source_file = "${local.lambdas_path}/s3/index.js"
+  output_path = "files/s3-artefact.zip"
+}
+
+resource "aws_lambda_function" "dynamo" {
+  function_name = "dynamo"
+  handler       = "index.handler"
+  role          = aws_iam_role.dynamo.arn
+  runtime       = "nodejs14.x"
+
+  filename         = data.archive_file.dynamo.output_path
+  source_code_hash = data.archive_file.dynamo.output_base64sha256
+
+  timeout     = 30
+  memory_size = 128
+
+  environment {
+    variables = {
+      TABLE = aws_dynamodb_table.this.name
+    }
+  }
 }
