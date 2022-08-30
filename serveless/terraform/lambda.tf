b5081f34eff51f960267ce51a432e7f91de8f68b
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-08-30 09:44:38 -0300

    Creating bucket and notification service

--- serveless/terraform/lambda.tf
+++ serveless/terraform/lambda.tf
@@ -38,6 +38,14 @@ resource "aws_lambda_function" "s3" {
   tags = local.common_tags
 }
 
+resource "aws_lambda_permission" "s3" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.todo.arn
+}
+
 data "archive_file" "dynamo" {
   type        = "zip"
   source_file = "${local.lambdas_path}/s3/index.js"
